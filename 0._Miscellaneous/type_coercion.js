1477ba19e4fa41a7427774fcf02ce4e53a03740d
Silke Marie <silk2918@stud.kea.dk>
2022-08-29 10:01:49 +0200

    intro

---
+++ 0._Miscellaneous/type_coercion.js
@@ -0,0 +1,11 @@
+//type coersion lecture
+const total = 34.32;
+const price = 12.32;
+
+const newTotal = total + price;
+console.log(newTotal);
+
+//tjekker her om total er det samme som newtotal.
+//her bruger vi 3 lighedstegn, hvis det er samme type
+const balanceEquals = total === newTotal;
+console.log(balanceEquals);
