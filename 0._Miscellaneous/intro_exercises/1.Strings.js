f18074a273365e1eb709159da39920a9196767b7
Silke Marie <silk2918@stud.kea.dk>
2022-08-29 13:33:16 +0200

    svar på lektier

--- 0._Miscellaneous/intro_exercises/1.Strings.js
+++ 0._Miscellaneous/intro_exercises/1.Strings.js
@@ -7,8 +7,8 @@ const numberTwo = "2.30";
 // add those two numbers and show the result
 // you cannot touch line 1 neither line 2
 
-const result = Number(numberOne) + Number(numberTwo);
-console.log(result);
+const result = parseFloat(numberOne) + parseFloat(numberTwo);
+console.log(result.toFixed(2));
 
 // --------------------------------------
 
@@ -20,7 +20,7 @@ const anotherNumberOne = "1.10";
 const anotherNumberTwo = "2.30";
 
 const anotherResult = Number(anotherNumberOne) + Number(anotherNumberTwo);
-console.log(anotherResult);
+console.log(anotherResult.toFixed(2));
 
 // --------------------------------------
 // Exercise 3 - Decimals and average
@@ -31,7 +31,10 @@ const three = 98;
 
 // Show in the console the avg. with 5 decimals
 
+const total = one + two + three;
+const totalAvg = total / 3;
 
+console.log(totalAvg.toFixed(5));
 
 
 
@@ -41,7 +44,7 @@ const three = 98;
 const letters = "abc";
 // Get me the character "c"
 
-
+console.log(letters[2]);
 
 
 // --------------------------------------
@@ -51,5 +54,6 @@ const fact = "You are learning javascript!";
 
 // capitalize the J in Javascript
 
+console.log(fact.replace('j', 'J'));
 
 // --------------------------------------
