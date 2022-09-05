002a74f670a40a949ad6f676281d3388d4e8af0c
Silke Marie <silk2918@stud.kea.dk>
2022-09-05 10:46:38 +0200

    noter fra session 2, med temaet variabler og funktioner, plus recap af express

--- 1._Basic_Express/app.js
+++ 1._Basic_Express/app.js
@@ -1,9 +1,30 @@
+//importerer express
 const express = require("express");
+
+//instantierer the library express
 const app = express();
 
+//another way to import in a single line:
+//const app = require("express")();
+
+// route    // callback function
 app.get("/", (request, response) => {
     response.send({ message: "Created my first route. Check." })
 });
+
+
+//alle vores routes bliver definere. express skaber tcp forbindelse til klienter. 
+//vi definerer noget kode der skal køre. 
+        // endpoint
+app.get("/deers", (req, res) => {
+    res.send({ size: "BIG" });
+});
+//^kan kalde req og res hvad som helst, men det er det de altid bliver kaldt. 
+
+app.get("/cups", (req, res) => {
+    res.send({ contains: "coffee" });
+});
+
 
 //GET tager imod to ting.
 //1: Endpointet
