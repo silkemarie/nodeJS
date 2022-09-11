74c137e025a602bbd0c4f165e20ce74f6d15a94a
Silke Marie <silk2918@stud.kea.dk>
2022-09-11 19:14:56 +0200

    npm install

--- weapons/app.js
+++ weapons/app.js
@@ -5,17 +5,19 @@ app.get("/", (req, res) => {
 });
 
 app.get("/weapons", (req, res) => {
-    res.send({ message: "Weapons" })
+    res.send({ message: "Weapons overview" })
 });
 
 app.get("/weapons/:id", (req, res) => {
-    console.log(req.params.id);
+    console.log(req.params);
     if (Number(req.params.id) === 1) {
         res.send({name: "Zweihander", isCool: true});
     } else {
         res.send({ errorMessage: "I don't know that weapon" })
     }
 });
+
+
 
 app.listen(8080);
 
