1f2f9bb5c49d124f5430ca404f7c3389515a91b6
Silke Marie <silk2918@stud.kea.dk>
2022-09-11 18:25:51 +0200

    id

--- weapons/app.js
+++ weapons/app.js
@@ -8,5 +8,15 @@ app.get("/weapons", (req, res) => {
     res.send({ message: "Weapons" })
 });
 
+app.get("/weapons/:id", (req, res) => {
+    console.log(req.params.id);
+    if (Number(req.params.id) === 1) {
+        res.send({name: "Gun", isAWeapon: true});
+    } else {
+        res.send({ errorMessage: "I don't know that deer" })
+    }
+});
+
+
 app.listen(8080);
 
