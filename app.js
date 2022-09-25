1b30d7e4e66d4d90f3a70b386b74ffe2c0f0e28a
Silke Marie <silk2918@stud.kea.dk>
2022-09-25 20:31:15 +0200

    changes to port for Heroku

--- app.js
+++ app.js
@@ -11,6 +11,4 @@ app.get("/", (req, res) => {
 
 
 
-app.listen(8080, () => {
-    console.log("Server is running on", 8080);
-});
+app.listen(process.env.PORT || 8080, () => {console.log("Server is running on port ", 8080)});
