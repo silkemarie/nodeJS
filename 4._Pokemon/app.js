45c315102999be361ad0d9beb0c77bdb3a8f134d
Silke Marie <silk2918@stud.kea.dk>
2022-09-26 10:56:33 +0200

    fetch excercises

--- 4._Pokemon/app.js
+++ 4._Pokemon/app.js
@@ -23,10 +23,33 @@ app.get("/pokemon", (req, res) => {
 */
 
 app.get("/pokemon", (req, res) => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+    .then(response => response.json())
+    .then(result => res.send({ data: result }));
+});
+
+
+/* Virker med frontend: 
+app.get("/pokemon", (req, res) => {
     console.log({ data: pokemon});
     res.send({ data: pokemon});
 })
+*/
 
-app.listen(8080, () => {
-    console.log("Server is running on", 8080);
+//const PORT = 8080;
+//const PORT = process.env.PORT ? process.env.PORT : 8080;
+const PORT = process.env.PORT || 8080;
+//^hvis process.env.PORT er defineret, så giv mig den, hvis ikke så giv mig 8080
+
+console.log(Number(process.env.PORT));
+//console.log(Number(process.env.PORT));
+
+
+
+
+const server = app.listen(process.env.PORT, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on", server.address().port);
 });
--- 4._Pokemon/public/frontpage.js
+++ 4._Pokemon/public/frontpage.js
@@ -1,3 +1,34 @@
+/*
 fetch("/pokemon")
     .then(response => response.json())
     .then(result => console.log(result));
+
+    */
+
+//const pokemonDiv = document.querySelector("#pokemon");
+//^løber igennem flere træer, hvor Id kun løber igennem id-træet
+
+setTimeout(function(){
+    window.location.href = "/battle";
+    //window.location.replace = "battle";
+ }, 3000);
+
+
+const pokemonDiv = document.getElementById("pokemon");
+
+
+fetch("/pokemon")
+.then(response => response.json())
+.then(result => {
+    result.data.results.forEach(pokemon => {
+        const pokemonIndividualDiv = document.createElement("div");
+        const pokemonNameP = document.createElement("p");
+        pokemonNameP.innerText = pokemon.name;
+
+
+
+        pokemonIndividualDiv.appendChild(pokemonNameP);
+        pokemonDiv.appendChild(pokemonIndividualDiv);
+
+    });
+});
