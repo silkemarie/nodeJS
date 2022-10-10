cce9f7bafa48f9354140b207af316d25840bda8f
Silke Marie <silk2918@stud.kea.dk>
2022-10-10 11:46:44 +0200

    svelte start

---
+++ 5._First_Svelte_Project/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Date from "./components/Date.svelte";
+
+  const name = "Silke";
+</script>
+
+<h2 id="page-title">Hello</h2>
+<div>more content for {name}.</div>
+<Date></Date>
+
+
+<style>
+  #page-title {
+    color: pink;
+  }
+</style>
